e26a60ebe5cffa61b24e677214b7872c9a756b1d
Alberto <ac.insite15@live.com>
2023-04-14 02:19:06 -0600

    adding changes in new views and update files for pubiish a web app priceapp wit a poll

--- pricesapp/poll/models.py
+++ pricesapp/poll/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # id - automatically django create a id
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateField("date published")
+    
+    # return text when is called
+    def __str__(self):
+        return self.question_text
+    
+    # return recently data 14 hours after today comparate 2 timezones
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+    
+    # return text when is called
+    def __str__(self):
+        return self.choice_text
--- pricesapp/poll/views.py
+++ pricesapp/poll/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
 
+# view name index, function based view
 def index(request):
-    return HttpResponse("Hello world")
+    lastest_question_list = Question.objects.all()
+    # return HttpResponse("You are in main page")
+    return render(request, "poll/index.html", {
+        "lastest_question_list":lastest_question_list
+    })
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking question # {question_id} ")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking results from question # {question_id} ")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting to question # {question_id} ")
 
