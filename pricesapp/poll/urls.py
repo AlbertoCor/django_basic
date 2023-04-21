605402a5569090d91d750604c5d64a7ea7e0b90c
Alberto <ac.insite15@live.com>
2023-04-21 12:34:03 -0600

    Adding update for error view page and files required

--- pricesapp/poll/urls.py
+++ pricesapp/poll/urls.py
@@ -2,12 +2,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = "poll"
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name = "index"),
-    #ex: /polls/5/
-    path("<int:question_id>/", views.detail, name = "index"),
-    #ex: /polls/5/results
-    path("<int:question_id>/results/", views.results, name = "index"),
-    #ex: /polls/5/vote
-    path("<int:question_id>/vote/", views.vote, name = "index"),
+    # ex: /polls/5/
+    path("<int:question_id>/detail/this_page_is_the_shit", views.detail, name = "detail"),
+    # ex: /polls/5/results
+    path("<int:question_id>/results/", views.results, name = "results"),
+    # ex: /polls/5/vote
+    path("<int:question_id>/vote/", views.vote, name = "vote"),
 ]
--- pricesapp/poll/views.py
+++ pricesapp/poll/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -12,7 +12,12 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking question # {question_id} ")
+    # question = Question.objects.get(pk=question_id) - Option 1
+    question = get_object_or_404(Question, pk= question_id)
+    # return HttpResponse(f"You are looking question # {question_id} ")
+    return render(request, "poll/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"You are looking results from question # {question_id} ")
